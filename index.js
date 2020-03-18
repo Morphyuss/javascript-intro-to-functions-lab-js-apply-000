63e1f9e7e0ce66ec3eeeaa18b623fcf4b55ee179
James Stamps Jr <jstampshere@gmail.com>
2020-03-18 16:07:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,14 +18,13 @@ function sayHiToGrandma(string) {
  if (string)===string.toLowerCase(); {
  return ("I can't hear you!");
     } else {
-    (string)===string.toUpperCase();{
+    (string)===string.toUpperCase();
     return ("YES INDEED!");
       } else {
       (string)===("I love you, Grandma.");
       return ("I love you, too.");
-  }  
+    }  
       }
-}
 
 
 /*
