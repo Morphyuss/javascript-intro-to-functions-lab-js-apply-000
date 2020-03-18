3c8c2dd0de5fe36d87c82bc15d963fc9f89082ab
James Stamps Jr <jstampshere@gmail.com>
2020-03-18 16:25:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,36 +25,3 @@ function sayHiToGrandma(string) {
 }    
 
 
-
-
-/*
-if (condition goes inside here){
-   // what to do if it's true
- } else if (condition goes here){
-   // what to do if it's true
-} else { // doesn't take a condition; it is a default
- // what to do in that case
-  }
-
-
-
-if (condition1) {
-  // block of code to be executed if condition1 is true
-} else if (condition2) {
-  // block of code to be executed if the condition1 is false and condition2 is true
-} else {
-  // block of code to be executed if the condition1 is false and condition2 is false
-}
-describe('sayHiToGrandma(string)', function() {
-  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-}) */
