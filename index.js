8c5b69e13ccb04d1ff42e5a6323154d35c928eed
James Stamps Jr <jstampshere@gmail.com>
2020-03-18 15:56:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,14 +15,25 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
- (string)===string.toLowerCase();
+ if (string)===string.toLowerCase(); {
  return ("I can't hear you!");
-} 
+} else if (string)===string.toUpperCase();{
+  return ("YES INDEED!");
+} else {
+  
+}
+}
 
 
 
-
-
+/*
+if (condition1) {
+  // block of code to be executed if condition1 is true
+} else if (condition2) {
+  // block of code to be executed if the condition1 is false and condition2 is true
+} else {
+  // block of code to be executed if the condition1 is false and condition2 is false
+}
 describe('sayHiToGrandma(string)', function() {
   it('returns "I can\'t hear you!" if `string` is lowercase', function() {
     expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
